087875f381cf19341b48a86051e3bb681fe9f2c7
yamadaippei <60645937+yamadaippei@users.noreply.github.com>
2020-03-18 12:15:39 +0900

    create-messages-path
    # What
    messagesのindexへのpathを追記しました。
    軽くビューの変更をしました。
    
    # Why
    上部のグループ一覧ボタンを押すことで
    一覧ページへ通るようにルーティングを記載。

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root 'lobi#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
+  resources :messages, only: [:index, :new, :create, :edit, :show]
+  resources :groups, only: [:index, :new, :creste]
 end
